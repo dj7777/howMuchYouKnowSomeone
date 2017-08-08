6219e40c6322a19ff7e8c03d98e783d434df0d71
codewithdj <deepak_jain@persistent.com>
2017-08-08 17:10:20 +0530

    structure is ready
    basic structure is ready... connectivity with mongodb is left

---
+++ src/js/slickQuiz-config.js
@@ -0,0 +1,72 @@
+// Setup your quiz text and questions here
+
+// NOTE: pay attention to commas, IE struggles with those bad boys
+
+var quizJSON = {
+    "info": {
+        "name":    "Test Your Knowledge!!",
+        "main":    "<p>Think you're smart enough to be on Jeopardy? Find out with this super crazy knowledge quiz!</p>",
+    //    "results": "<h5>Learn More</h5><p>Etiam scelerisque, nunc ac egestas consequat, odio nibh euismod nulla, eget auctor orci nibh vel nisi. Aliquam erat volutpat. Mauris vel neque sit amet nunc gravida congue sed sit amet purus.</p>",
+        "level1":  "BFFFFFF",
+        "level2":  "Best Friend ",
+        "level3":  "Just a friend",
+        "level4":  "Ek chakkar wale friend.",
+        "level5":  "You know nothing, Jon Snow" // no comma here
+    },
+    "questions": [
+        { // Question 1 - Multiple Choice, Single True Answer
+            "q": "What's your favorite color?",
+            "a": [
+                {"option": "Blue",      "correct": false},
+                {"option": "Red",     "correct": false},
+                {"option": "Green",      "correct": true},
+                {"option": "White",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> Green!</p>",
+            "incorrect": "<p><span>Uhh no.</span></p>" // no comma here
+        },
+        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+            "q": "Which of the following best represents your preferred breakfast?",
+            "a": [
+                {"option": "Sprouts and Sandwich",               "correct": false},
+                {"option": "Fruit, oatmeal, and yogurt",   "correct": true},
+                {"option": "Leftover pizza",               "correct": false},
+                {"option": "Poha & Jalebi", "correct": true} // no comma here
+            ],
+            "select_any": true,
+            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
+            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+        },
+        { // Question 3 - Multiple Choice, Multiple True Answers, Select All
+            "q": "Where are you right now? Select ALL that apply.",
+            "a": [
+                {"option": "Planet Earth",           "correct": true},
+                {"option": "Pluto",                  "correct": false},
+                {"option": "At a computing device",  "correct": true},
+                {"option": "The Milky Way",          "correct": true} // no comma here
+            ],
+            "correct": "<p><span>Brilliant!</span> You're seriously a genius, (wo)man.</p>",
+            "incorrect": "<p><span>Not Quite.</span> You're actually on Planet Earth, in The Milky Way, At a computer. But nice try.</p>" // no comma here
+        },
+        { // Question 4
+            "q": "Favourite Pet?",
+            "a": [
+                {"option": "Cat",    "correct": false},
+                {"option": "Dog",     "correct": true},
+                {"option": "Panda",      "correct": false},
+                {"option": "Tiger",   "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Holy bananas!</span> I didn't actually expect you to know that! Correct!</p>",
+            "incorrect": "<p><span>Fail.</span> Sorry. You lose.</p>" // no comma here
+        },
+        { // Question 5
+            "q": "Is Earth bigger than a basketball?",
+            "a": [
+                {"option": "Yes",    "correct": true},
+                {"option": "No",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>Good Job!</span> You must be very observant!</p>",
+            "incorrect": "<p><span>ERRRR!</span> What planet Earth are <em>you</em> living on?!?</p>" // no comma here
+        } // no comma here
+    ]
+};
